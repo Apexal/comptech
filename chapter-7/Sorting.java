0ff8f8b91c81186332fc0a9b33e03d493432c3d3
Frank Matranga <thefrankmatranga@gmail.com>
2016-05-09 21:35:03 -0400

    Added bubble sort

--- chapter-7/Sorting.java
+++ chapter-7/Sorting.java
@@ -14,6 +14,16 @@ public class Sorting {
 		return index;
 	}
 	
+	static boolean isSorted(int[] a) {
+		boolean sorted = true;
+		for(int i = 0; i < a.length - 1; i++) {
+			if(a[i] > a[i + 1])
+				sorted = false;
+		}
+		
+		return sorted;
+	}
+	
 	static int[] selectionSort(int[] a) {
 		for(int i = 0; i < a.length - 1; i++) {
 			int iOfNextSm = getIndexOfSmallest(i, a);
@@ -27,13 +37,31 @@ public class Sorting {
 		return a;
 	}
 	
+	static int[] bubbleSort(int[] a) {
+		while(!isSorted(a)) {
+			for(int i = 0; i < a.length - i; i++) {
+				if(a[i] > a[i + 1]) {
+					// Swap the two
+					int temp = a[i];
+					a[i] = a[i + 1];
+					a[i + 1] = temp;
+				}
+			}
+		}
+		
+		return a;
+	}
 	
 	public static void main(String[] args) {
 		int[] nums = {3, 5, 1, 6, 89, -1};
 		
 		int[] ordered = selectionSort(nums);
-		
 		for(int item : ordered) {
+			System.out.println(item);
+		}
+		
+		int[] ordered2 = bubbleSort(nums);
+		for(int item : ordered2) {
 			System.out.println(item);
 		}
 	}
